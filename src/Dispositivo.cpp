1cc0efa1b3bade8dadc9cbc756df04a7e9fffae2
Steva0 <michele.stevanin@gmail.com>
2024-12-30 20:01:46 +0100

    modifica show all

--- src/Dispositivo.cpp
+++ src/Dispositivo.cpp
@@ -74,8 +74,7 @@ std::string Dispositivo::showSmall() const{
 }
 
 std::string Dispositivo::showConsumo() const{
-    std::string info = "Nome: " + getNome() + "\n"
-                     + "Consumo energetico: " + std::to_string(calcolaConsumoEnergetico()) + " kWh";
+    std::string info = getNome() + " " + std::to_string(calcolaConsumoEnergetico()) + " kWh";
     return info;
 }
 
