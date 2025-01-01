add62138c09439a9a8ae39e194df473629d9dea1
Steva0 <michele.stevanin@gmail.com>
2025-01-01 19:53:53 +0100

    Aggiunta di nuovi dispositivi alla lista

--- include/ListaDispositivi.h
+++ include/ListaDispositivi.h
@@ -27,7 +27,22 @@ const std::vector<std::pair<std::string, std::tuple<double, int, bool>>> disposi
     {"Wii", {-0.4, 0, false}},                    //add nostre
     {"Piano a induzione", {-1.5, 0, false}},      //add nostre
     {"Allarme", {-0.4, 0, false}},                //add nostre
-    {"Citofono", {-0.2, 0, true}}
+    {"Citofono", {-0.2, 0, true}},                //add nostre
+    {"Condizionatore", {-1.8, 300, false}},       //add nostre
+    {"Stufa elettrica", {-2.5, 0, false}},        //add nostre
+    {"Lampada LED", {-0.05, 0, false}},           //add nostre
+    {"Proiettore", {-1.0, 120, false}},           //add nostre
+    {"Stereofono", {-0.3, 30, false}},            //add nostre
+    {"Macchina del caffè", {-0.2, 2, false}},     //add nostre
+    {"Friggitrice ad aria", {-1.2, 100, false}},  //add nostre
+    {"Deumidificatore", {-1.0, 50, true}},        //add nostre
+    {"Ventilatore", {-0.6, 0, false}},            //add nostre
+    {"Tostapane", {-0.3, 5, false}},              //add nostre
+    {"Piastra per capelli", {-0.1, 10, false}},   //add nostre
+    {"Microfono", {-0.05, 0, false}},             //add nostre
+    {"Amplificatore", {-0.8, 50, false}},         //add nostre
+    {"Rasaerba elettrico", {-1.5, 0, false}},     //add nostre
+    {"Macchina per il pane", {-0.8, 150, false}}  //add nostre
 };
 
 // Mappa per tenere traccia del numero di serie specifico per ogni tipo di dispositivo
