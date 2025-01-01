68d0d5ad8fe14e92c3dddd78112533acfebd7b29
Just <fabri.scabbia@gmail.com>
2025-01-01 23:02:02 +0100

    Aggiunta nuove funzioni di controllo per la gestione dei dispositivi e semplificazione delle firme esistenti

--- include/Interfaccia.h
+++ include/Interfaccia.h
@@ -7,18 +7,20 @@ class Interfaccia
 {
 private:
     const double MAX_KILOWATT = 3.5;
+    void checkTurnOnDevices(int currentTime, bool& cambiatoQualcosa);
+    void checkTurnOffDevices(int currentTime, bool& cambiatoQualcosa);
 
-    void turnOffDevices(LinkedList& dispositiviAccesi, LinkedListOff& dispositiviSpenti, int currentTime, bool& cambiatoQualcosa);
-    void checkKilowatt(LinkedList& dispositiviAccesi, LinkedListOff& dispositiviSpenti, int currentTime, bool& blackout, bool& cambiatoQualcosa);
-    void changeDeviceStatus(std::string newStatus, std::string nomeDispositivo, LinkedList& dispositiviAccesi, LinkedListOff& dispositiviSpenti, int currentTime);
+    void checkKilowatt(int currentTime, bool& blackout, bool& cambiatoQualcosa);
+    void changeDeviceStatus(std::string newStatus, std::string nomeDispositivo, int currentTime);
     void setDeviceTimer(Dispositivo& dispositivo, int startTime, int endTime);
-    void commandSetDeviceTimer(int startTime, int endTime, std::string nomeDispositivo, LinkedList& dispositiviAccesi, LinkedListOff& dispositiviSpenti, int currentTime);
-    void handleDeviceHasAlreadyTimer(std::string nomeDispositivo, int startTime, int endTime, LinkedList& dispositiviAccesi, int currentTime);
+    void commandSetDeviceTimer(int startTime, int endTime, std::string nomeDispositivo, int currentTime);
+    void handleDeviceHasAlreadyTimer(std::string nomeDispositivo, int startTime, int endTime, int currentTime);
     
     int convertTimeToInt(std::string time);
     std::string convertIntToTime(int minuti);
     LinkedList dispositiviAccesi = LinkedList();
     LinkedListOff dispositiviSpenti = LinkedListOff();
+    LinkedList dispositiviDaAccendere = LinkedList();
     int currentTime = 0;
     std::string possibleCommands[5] = {"set", "rm", "show", "reset", "help"};
     
