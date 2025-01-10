9e4ef3b51de7e091e52ce61b4b4450efb6d52757
Steva0 <michele.stevanin@gmail.com>
2025-01-10 15:48:55 +0100

    Aggiunti commenti

--- include/Dispositivo.h
+++ include/Dispositivo.h
@@ -17,14 +17,14 @@ private:
     static int lastId;
 
 protected:
-    std::string nome;
-    int id;           //Id univoco per ogni dispositivo
+    std::string nome;   // Nome del tipo di dispositivo
+    int id;           // Id univoco per ogni dispositivo
     int numeroSerie;  // Numero di serie specifico per il tipo di dispositivo
     double potenza;         // Positivo per produzione, negativo per consumo
     bool sempreAcceso;      // Indica se il dispositivo non può mai essere spento
-    int orarioAccensione;   // In minuti da 0 a 1439
-    int orarioSpegnimento;  // In minuti da 0 a 1439
-    int durataCiclo;        //se manuale = 0 se CP != 0
+    int orarioAccensione;   // In minuti da 0 (00:00) a 1439 (23:59)
+    int orarioSpegnimento;  // In minuti da 0 (00:00) a 1439 (23:59)
+    int durataCiclo;        //se manuale == 0 se CP != 0
     int tempoAccensione;    //tempo tot accensione
     bool timerOn;         //true se timer attivo
 
@@ -32,20 +32,23 @@ public:
     // Costruttore
     Dispositivo();
     Dispositivo(const std::string& nome, double potenza, int durataCiclo, bool sempreAcceso = false, int orarioAccensione = 0, int orarioSpegnimento = 0, bool hasTimer = false);
-    // Distruttore virtuale
-    virtual ~Dispositivo() = default;
-    // Operatore di assegnazione
+    ~Dispositivo() = default;
+    // Operatore di copia
     Dispositivo& operator=(const Dispositivo& other);
 
-    //metodi generali
-    double calcolaConsumoEnergetico() const;
+
+    //funzioni generali
+
+    double calcolaConsumoEnergetico() const;        //calcola i kW consumati in totale in kWh
     std::string showAllInfo() const;                //nome + numeroSerie, id,  potenza, sempreAcceso, orarioAccensione, orarioSpegnimento, tempoAccensione
     std::string showInfo() const;                   //nome + numeroSerie, id, tempoAccensione
     std::string showSmall() const;                  //nome + numeroSerie, id
     std::string showConsumo() const;                //nome + numeroSerie, consumo energetico
     std::string showName() const;                   //nome + numeroSerie
 
+
     //getter
+
     std::string getTipo() const;                    //nome del tipo di dispositivo
     std::string getNome() const;                    //nome + numeroSerie
     int getId() const;                              //id
@@ -56,25 +59,28 @@ public:
     int getOrarioSpegnimento() const;               //orarioSpegnimento
     int getTempoAccensione() const;                 //tempoAccensione
     int getDurataCiclo() const;                     //durataCiclo
-    bool isManual() const;                      
-    bool isCP() const;
+    bool isManual() const;                          //true se durataCiclo == 0, false se durataCiclo != 0
+    bool isCP() const;                              //true se durataCiclo != 0, false se durataCiclo == 0
     void setTimerOff();                             //rimuove timer di spegnimento, solo ai manuali
     bool isAcceso(int currentTime) const;           //true se acceso, false se spento
     bool isGenerator() const;                       //true se generatore, false se consumatore
     bool hasTimer() const;                          //restituisce true se il dispositivo ha un timer attivo
 
+
     //setter
-    void setOrarioAccensione(int minuti, bool isTimer = false);           // può lanciare eccezione std::invalid_argument
-    void setOrarioSpegnimento(int minuti, bool isTimer = false);          // può lanciare eccezione std::invalid_argument
-    void incrementaTempoAccensione(int minuti);     //può lanciare eccezione std::invalid_argument
-    void resetTempoAccensione();                    //resetta tempoAccensione a 0
-    void setHasTimer(bool hasTimer);                //setta boolTimer
+    
+    void setOrarioAccensione(int minuti, bool isTimer = false);           //setta orarioAccensione                 
+    void setOrarioSpegnimento(int minuti, bool isTimer = false);          //setta orarioSpegnimento
+    void incrementaTempoAccensione(int minuti);                           //incrementa tempoAccensione                 
+    void resetTempoAccensione();                                          //resetta tempoAccensione a 0
+    void setHasTimer(bool hasTimer);                                      //setta boolTimer
 
     //variabili statiche
+
     static const int MINUTI_GIORNATA = 1440;
     static const int MAX_MINUTI_GIORNATA = 1439;
 };
 
 #endif // DISPOSITIVO_H
 
-std::string trasformaOrario(int minuti);
+std::string trasformaOrario(int minuti);                
