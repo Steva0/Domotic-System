da82bb6567aca549dd53f12819983cd08744077c
MochaSummer2588 <albybort04@gmail.com>
2025-01-11 14:00:51 +0100

    Rinomina la funzione getActualConsumption in getCurrentConsumption per maggiore chiarezza

--- include/LinkedListOn.h
+++ include/LinkedListOn.h
@@ -20,7 +20,7 @@ class LinkedListOn: public LinkedList
     std::vector<Dispositivo> removeAllDevicesOff(const int currentTime);                //rimuove tutti i dispositivi spenti (il cui orario di spegnimento e' prima dell'orario indicato) e li restituisce tutti in un vector            
 
     void incrementTimeOn();                                                             //incrementa il tempo di tutti i dispositivi accesi in un minuto - Può lanciare eccezione std::invalid_argument e std::out_of_range                
-    double getActualConsumption() const;                                                   //restituisce il consumo totale        
+    double getCurrentConsumption() const;                                                //restituisce il consumo totale        
     double producedEnergy() const;                                                      //restituisce l'energia prodotta in questo minuto
     double consumedEnergy() const;                                                      //restituisce il modulo dell'energia consumata in questo minuto
 };
